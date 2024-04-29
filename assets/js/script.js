f7c4f028cf60711f7afc0f27be8e38292d1ead72
unknown <aykhanaa@code.edu.az>
2024-04-30 03:09:14 +0400

    write academywork code

---
+++ assets/js/script.js
@@ -0,0 +1,185 @@
+"use strict";
+
+
+// setTimeout(() => {
+//     // alert("Salam Resad")
+//     window.location.reload();
+// }, 3000);
+
+// function checkOddNum(num){
+//     return num % 2 ==1;
+// }
+
+// function checkEvenNum(num){
+//     return num % 2 ==0;
+// }
+
+
+// function sumofNumsbyConditions(arr, callback) {
+//     let sum = 0;
+//     for (const item of arr) {
+//         if (callback(item)) {
+//             sum += item
+//         }
+//     }
+//     console.log(sum);
+// }
+
+
+// let nums = [1, 4, 5, 6, 9, 8];
+// sumofNumsbyConditions(nums, m => m % 2 == 0)
+// sumofNumsbyConditions(nums, m => m > 5)
+
+// let nums = [1, 4, 5, 6, 9, 8];
+
+// let result = nums.filter(m=>m>5);
+
+// let result = nums.find(m=>m>5);
+// console.log(result);
+
+// let result = nums.findIndex(m=>m==5);
+// console.log(result);
+
+
+// class Animal{
+//     constructor(height,width){
+//         this.height = height;
+//         this.width = width;
+//     }
+// }
+
+// // let animal = new Animal(100,200);
+
+// class Bird extends Animal{
+//     constructor(a,b){
+//         super{100,200};
+//         this.name = "Devequsu"
+//     }
+// }
+
+// getName(t){
+//     console.log("ttes");
+// }
+
+
+// let Bird = new Bird();
+// console.log(100);
+
+// let name = "sasa";
+
+// let surname = new String ();
+
+// surname = 100
+
+// console.log();surname
+
+// let isMarried = new Boolean();
+
+// isMarried = true
+
+// console.log(isMarried);
+
+// class Test{
+//     constructor(name){
+//         this.name = name;
+//     }
+// }
+
+// Test,prototype.surname = "Surname for test"
+
+// Test,prototype.getName = function(){
+//     console.log("this is a test name");
+// }
+
+// let test = new Test("salam")
+// test.getName();
+
+// String.prototype.customContains = function(serachText){
+//     return this.includes(serachText)
+// }
+// let name = "Reshad";
+
+// let surname = "Agayev";
+
+
+// console.log(name.customContains("c"));
+
+// console.log(surname.customContains("v"));
+
+// let data = new Date();
+
+// let year = date.getFullYear();
+
+// let month = date.getUTCMouth();
+
+// let result = `${month -2} - ${year}`;
+
+// console.log(result);
+
+// let data = Math.sqrt(64)
+// console.log(data);
+
+
+// DOM
+
+// let elems = document.getElementsByTagName("h1");
+
+// for (let i = 0; i < elems.length; i++) {
+//     const element = elems[i];
+//     console.log(element);
+    
+// }
+// let elem = document.getElementsByClassName("test");
+// console.log(elem);
+
+
+//important
+// let elem = document.getElementById("products")
+// console.log(elem);
+
+// let elems = document.querySelectorAll("#products h1")
+// elems[1].style.backgroundColor = "Red"
+
+// for (const item of elems) {
+//     item.style.backgroundColor = "Red"
+    
+// }
+
+let h1 = document.querySelector("h1");
+
+// console.log(h1.innerText);
+
+// console.log(h1.innerHTML);
+// h1.innerHTML = "<span>Reshad bey</span>"
+
+// h1.style.color = "Teal";
+// h1.style.margin = "200px";
+
+// h1.className = "active";
+
+// h1.classList.add("active")
+// h1.classList.remove("product")
+
+// console.log(h1.classList.contains("active"));
+
+// h1.setAttribute("id","text")
+// console.log(h1.getAttribute("id"));
+// console.log(h1.hasAttribute("id"));
+
+// let button = document .querySelector(".add-product");
+
+// let = document. querySelector("h1");
+
+// button-addEventListener ("click", function(){
+//  this.style.backgroundColour = "Magenta";
+
+
+// h1.style.backgroundColour = "Green";
+// h1.style.colour = "White";
+// h1. hi.style.width = "200px"
+// let btnText = this.innerText;
+// this,innerText = h1.innerText;
+// h1.innerText = btnText;
+
+// })
+
